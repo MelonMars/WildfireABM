e49dba665195cfe1c5892e2b35c9c8dfe8c11115
Carter Pfaff <79235133+MelonMars@users.noreply.github.com>
2024-07-17 21:53:23 -0400

    Another hour
    Decent amount

--- main.py
+++ main.py
@@ -1,25 +1,97 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import vnoise
-from scipy.ndimage import gaussian_filter
+import math
+
 
 # Setup:
 
 
 class GridPoint:
-    def __init__(self, x, y, species, moisture, elev, fuel, density, state):
+    def __init__(self, x, y, veg, moisture, elev, fuel, density, state, length=10):
+        """
+        :param x:
+        :param y:
+        :param species:
+        :param moisture:
+        :param elev:
+        :param fuel:
+        :param density:
+        :param state:
+            State 0: Nothing flammable ever. Won't burn at all (e.g. roads, water)
+            State 1: Flammable, but not currently burning
+            State 2: Started burning just now
+            State 3: Currently burning
+            State 4: Burned out
+        """
         self.x = x
         self.y = y
-        self.species = species
+        self.veg = veg
         self.moisture = moisture
         self.elev = elev
         self.fuel = fuel
         self.density = density
         self.state = state
+        self.length = length
+        self.propFac = None
 
+    def step(self, gpGrid: np.ndarray, windDir: float, windSpeed):
+        if self.state == 0:
+            self.state = 0
+        elif self.state == 1:
+            self.state = 1
+        elif self.state == 2:
+            self.state = 3
+            self.spreadFire(gpGrid, windDir, windSpeed)
+        elif self.state == 3:
+            self.state = 4
 
-def linearGradient(a: float, b: float, x: float, y: float) -> (float, float):
-    return a * x + b * y
+    def spreadFire(self, gpGrid: np.ndarray, windDir: float, windSpeed: float):
+        """
+        Spread fire to neighboring cells
+        Vegetation factor is from 1-3, 1 agricultural, 2 shrubs, 3 trees
+        Density factor is from 1-3, 1 sparse, 2 medium, 3 dense
+        Wind factor:
+            c1 is a constant
+            c2 is a constant
+            V is the wind speed
+            theta is the angle between fire direction and wind direction
+        Slope factor:
+            a is a constant
+            thetaS is the slope angle of the path
+        :return:
+        """
+
+        directions_horizontal_vertical = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        directions_diagonal = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        neighbors_hor_ver = [gpGrid[self.x + dx, self.y + dy] for dx, dy in directions_horizontal_vertical if 0 <= self.x + dx < width and 0 <= self.y + dy < height]
+        neighbors_dia = [gpGrid[self.x + dx, self.y + dy] for dx, dy in directions_diagonal if 0 <= self.x + dx < width and 0 <= self.y + dy < height]
+        c1 = 0
+        c2 = 0
+        V = windSpeed
+        theta = self.propFac - windDir
+        a = 0
+        pH = 0  # Constant factor
+        pW = np.exp(np.dot(c1, V)) * np.exp(np.dot(V, np.dot(c2, (math.cos(theta) - 1))))
+        for tile in neighbors_hor_ver:
+            thetaS = math.atan((tile.elev - self.elev) / self.length)
+            pVeg = tile.veg # Vegetation factor
+            pDen = tile.density # Density factor
+            pS = np.exp(np.dot(a,thetaS)) # Slope factor
+            pBurn = pH * (1 + pVeg) * (1 + pDen) * pW * pS
+            if (np.random.randint(0, 1) > pBurn):
+                tile.state = 2
+                # Set tile propFac to the propagation factor from here (i.e. tell the tile the angle the fire came at it from)
+
+        for tile in neighbors_dia:
+            thetaS = math.atan((tile.elev - self.elev) / self.length*math.sqrt(2))
+            pVeg = tile.veg  # Vegetation factor
+            pDen = tile.density  # Density factor
+            pS = np.exp(np.dot(a, thetaS))  # Slope factor
+            pBurn = pH * (1 + pVeg) * (1 + pDen) * pW * pS
+            if (np.random.randint(0, 1) > pBurn):
+                tile.state = 2
+                # Set tile propFac to the propagation factor from here (i.e. tell the tile the angle the fire came at it from)
 
 
 width, height = 10, 10
